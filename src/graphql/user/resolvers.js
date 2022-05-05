8bf8a30e55eb62278214d443493680d1070b6477
RenanBrickes <renan.brickes@fatec.sp.gov.br>
2022-05-05 12:36:00 -0300

    Adicionando relação de post em query de user

--- src/graphql/user/resolvers.js
+++ src/graphql/user/resolvers.js
@@ -1,18 +1,20 @@
 const users = async (_, { input }, { getUsers }) => {
-    const apiFiltersInput = new URLSearchParams(input);
-    const users = await getUsers('/?' + apiFiltersInput);
-    return users.json();
-  };
-  
-  const user = async (_, { id }, { getUsers }) => {
-    const response = await getUsers('/' + id);
-    const user = await response.json();
-    return user;
-  };
-  
-  export const userResolvers = {
-    Query: {
-      user,
-      users,
-    },
-  };
+  const apiFiltersInput = new URLSearchParams(input);
+  const users = await getUsers('/?' + apiFiltersInput);
+  return users.json();
+};
+
+const user = async (_, { id }, { getUsers }) => {
+  const response = await getUsers('/' + id);
+  const user = await response.json();
+  return user;
+};
+
+const posts = ({ id }, _, { postDataLoader }) => {
+  return postDataLoader.load(id);
+};
+
+export const userResolvers = {
+  Query: { user, users },
+  User: { posts },
+};
