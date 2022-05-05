e7ebbf627809eb3de11561d2a9ac98ed80e411b0
RenanBrickes <renan.brickes@fatec.sp.gov.br>
2022-05-05 12:25:33 -0300

    Refatorando dataloaders

--- src/graphql/context.js
+++ src/graphql/context.js
@@ -1,7 +1,12 @@
-import fetch from "node-fetch";
+import fetch from 'node-fetch';
+import { getPosts } from './post/utils';
+import { getUsers } from './user/utils';
+import { makeUserDataLoader } from './user/dataloaders';
+
 export const context = () => {
-    return {
-        getUsers: (path = '/') => fetch('http://localhost:3000/users' + path),
-        getPosts: (path = '') => fetch('http://localhost:3000/posts' + path)
-    }
-}
+  return {
+    userDataLoader: makeUserDataLoader(getUsers(fetch)),
+    getUsers: getUsers(fetch),
+    getPosts: getPosts(fetch),
+  };
+};
