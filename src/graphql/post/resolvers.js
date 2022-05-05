16982cef992fb95068a19d8cb85d915cfa13f53f
RenanBrickes <renan.brickes@fatec.sp.gov.br>
2022-05-05 12:07:41 -0300

    Adicionando resolver para consulta n+ 1

--- src/graphql/post/resolvers.js
+++ src/graphql/post/resolvers.js
@@ -1,20 +1,21 @@
 const post = async (_, { id }, { getPosts }) => {
-    const response = await getPosts('/' + id);
-    return response.json();
-  };
-  
-  const posts = async (_, { input }, { getPosts }) => {
-    const apiFiltersInput = new URLSearchParams(input);
-    const response = await getPosts('/?' + apiFiltersInput);
-    return response.json();
-  };
-  
-  export const postResolvers = {
-    Query: { post, posts },
-    Post: {
-      unixTimestamp: ({ createdAt }) => {
-        const timestamp = new Date(createdAt).getTime() / 1000;
-        return Math.floor(timestamp);
-      },
-    },
-  };
+  const response = await getPosts('/' + id);
+  const post = await response.json();
+  return post;
+};
+
+const posts = async (_, { input }, { getPosts }) => {
+  const apiFiltersInput = new URLSearchParams(input);
+  const response = await getPosts('/?' + apiFiltersInput);
+  return response.json();
+};
+
+const user = async ({ userId }, _, { getUsers }) => {
+  const response = await getUsers('/' + userId);
+  return response.json();
+};
+
+export const postResolvers = {
+  Query: { post, posts },
+  Post: { user },
+};
